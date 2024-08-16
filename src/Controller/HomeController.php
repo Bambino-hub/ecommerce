a1b7b3a120cc37738af059fc0885a1a94f55b48f
bambino <bambinohoberenak@gmail.com>
2024-08-16 08:46:09 +0000

    filtraion des produits en fonction des sous categories

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Entity\SubCategory;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'ecommerce.home')]
-    public function index(): Response
-    {
+    #[Route('/', name: 'ecommerce.home', methods: ['GET'])]
+    public function index(
+        ProductRepository $productRepository,
+        CategoryRepository $categoryRepository
+    ): Response {
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $productRepository->findAll(),
+            'categories' => $categoryRepository->findAll()
+        ]);
+    }
+
+    #[Route('/home/show/{id}', name: 'ecommerce.home.show', methods: ['GET'])]
+    public function show(Product $product): Response
+    {
+        return $this->render('home/home.show.html.twig', [
+            'product' => $product
+        ]);
+    }
+
+    #[Route('/home/product/subcategorie/{id}', name: 'ecommerce.product.subcategorie', methods: ['GET'])]
+    public function showProductBySubCategorie(SubCategory $subCategory): Response
+    {
+        $products = $subCategory->getProducts();
+        return $this->render('home/product.subcategorie.html.twig', [
+            'products' => $products,
+            'subcategorie' => $subCategory
         ]);
     }
 }
